fd376bb8b08351c6958c6a526d6b829b97685895
tdeheurles <tdeheurles@gmail.com>
2015-12-19 22:10:26 +0100

    ensure run permission

--- circleci/ensurePermissions.sh
+++ circleci/ensurePermissions.sh
@@ -8,4 +8,4 @@ chmod 755 circleci/runContainers.sh
 chmod 755 circleci/pushContainers.sh
 chmod 755 src/build/kubernetes/push.sh
 chmod 755 src/build/kubernetes/build.sh
-chmod 755 src/build/kubernetes/run.sh
+chmod 755 src/run/kubernetes/run.sh
