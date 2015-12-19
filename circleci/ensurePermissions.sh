fbc9065cc8f1c749d07b838360bd5cb4e632fb59
tdeheurles <tdeheurles@gmail.com>
2015-12-19 21:40:55 +0100

    Circleci start containers

--- circleci/ensurePermissions.sh
+++ circleci/ensurePermissions.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 cd ..
 chmod 755 circleci/buildContainers.sh
+chmod 755 circleci/runContainers.sh
 chmod 755 circleci/pushContainers.sh
 chmod 755 src/build/kubernetes/push.sh
 chmod 755 src/build/kubernetes/build.sh
