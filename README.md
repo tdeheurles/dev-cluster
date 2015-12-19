1fb30ee2bef72f2e8f43743a9e443f8525f1b194
tdeheurles <tdeheurles@gmail.com>
2015-12-19 21:27:12 +0100

    Add push to circleci

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dev-cluster
+
+[![Circle CI](https://circleci.com/gh/tdeheurles/dev-cluster.svg?style=svg)](https://circleci.com/gh/tdeheurles/dev-cluster)
--- circleci/pushContainers.sh
+++ circleci/pushContainers.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+
 cd ../src/build/kubernetes
 ./push.sh $CIRCLE_BUILD_NUM
 cd ../../..
